c1c4e6d48a37de57bc8900f4a5912b3561d958c3
Jason Melgoza <jasonmelgoza@gmail.com>
2024-05-16 15:48:29 -0700

    Updated config for css and js

--- playroom.config.js
+++ playroom.config.js
@@ -1,9 +1,16 @@
+const path = require('path')
+const commonIncludes = [
+  path.resolve(__dirname, './'),
+  path.resolve(__dirname, './src/components'),
+]
+
 module.exports = {
   components: './src/components',
   outputPath: './dist/playroom',
 
   // Optional:
   title: 'Oblivion Playroom',
+  frameComponent: './playroom/Frame.js',
   widths: [320, 768, 1024],
   port: 9000,
   openBrowser: true,
@@ -13,4 +20,27 @@ module.exports = {
     </Button>
   `,
   baseUrl: '/playroom/',
-};
+  webpackConfig: () => ({
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          include: commonIncludes,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        },
+        {
+          test: /\.css$/i,
+          include: path.resolve(__dirname, './src/styles'),
+          exclude: /node_modules/,
+          use: [require.resolve('style-loader'), require.resolve('css-loader')],
+        },
+      ],
+    },
+  }),
+}
---
+++ playroom/Frame.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import '../src/styles/reset.css'
+
+export default function Frame({ children }) {
+  return <>{children}</>
+}
