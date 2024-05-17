aed979f7baa79f7aca547240cc2b561ae2f2da56
Jason Melgoza <jasonmelgoza@gmail.com>
2024-05-17 12:05:18 -0700

    Updated component index

--- src/components/Button/Button.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { Button as StyledButton } from '@radix-ui/themes'
-
-export default function Button({ children }) {
-  return <StyledButton>{children}</StyledButton>
-}
